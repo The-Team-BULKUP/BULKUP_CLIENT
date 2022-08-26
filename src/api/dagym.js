5a9d9c3f94cc460217e832bc27f5879bd02de024
PENEKhun <penekhun@gmail.com>
2022-08-26 11:30:54 +0900

    fix: 헬스장 정보가 돌아오지 않았던 버그 수정

--- src/api/dagym.js
+++ src/api/dagym.js
@@ -2,10 +2,6 @@ import axios from "axios";
 
 export const Dagym = {
     fetchGymListByName: async (gymName) => {
-        return axios.get(`${process.env.VUE_APP_API_HOST}_/gymSearch/${gymName}`);
-        // return gymListNameAxios.get("", {headers : { "Content-Type": "multipart/form-data"}});
+        return axios.get(`${process.env.VUE_APP_API_HOST}/gymSearch/${gymName}`);
     },
-    // fetchGymListByPoint: async (account) => {
-    //     // return dagymAxios.get("", account, {headers : { "Content-Type": "multipart/form-data"}});
-    // },
 }
